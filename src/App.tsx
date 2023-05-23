a7aea3f7722d8c18c4d55900054b1dc2665ea3e7
donoh <201904014@o.cnu.ac.kr>
2023-05-23 20:22:18 +0900

    feat : Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { MuiTextField } from "./components/MuiTextField";
+import { MuiSelect } from "./components/MuiSelect";
+// import { MuiTextField } from "./components/MuiTextField";
 // import { MuiButton } from "./components/MuiButton";
 // import { MultiTypography } from "./components/MultiTypography";
 
@@ -8,7 +9,8 @@ function App() {
         <div className="App">
             {/* <MultiTypography /> */}
             {/* <MuiButton /> */}
-            <MuiTextField />
+            {/* <MuiTextField /> */}
+            <MuiSelect />
         </div>
     );
 }
