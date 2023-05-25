a2259b0ff1c11d15f0be1ab69f6839dc6926047f
donoh <201904014@o.cnu.ac.kr>
2023-05-25 11:09:14 +0900

    feat : Grid

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,36 +1,60 @@
-import { Box, Divider, Stack } from "@mui/material";
+import { Box, Divider, Grid, Stack } from "@mui/material";
 
 export const MuiLayout = () => {
     return (
-        <Stack
-            sx={{ border: "1px solid" }}
-            direction={"row"}
-            spacing={2}
-            divider={<Divider orientation="vertical" flexItem />}
-        >
-            <Box
-                sx={{
-                    backgroundColor: "primary.main",
-                    color: "white",
-                    width: "200px",
-                    height: "100px",
-                    padding: "16px",
-                    "&:hover": {
-                        backgroundColor: "primary.light",
-                        transition: "background-color 0.5s ease",
-                    },
-                    transition: "background-color 0.5s ease",
-                }}
+        <>
+            <Stack
+                sx={{ border: "1px solid" }}
+                direction={"row"}
+                spacing={2}
+                divider={<Divider orientation="vertical" flexItem />}
             >
-                Codevolution
-            </Box>
-            <Box
-                display="flex"
-                height="100px"
-                width="200px"
-                bgcolor={"success.light"}
-                p={2}
-            ></Box>
-        </Stack>
+                <Box
+                    sx={{
+                        backgroundColor: "primary.main",
+                        color: "white",
+                        width: "200px",
+                        height: "100px",
+                        padding: "16px",
+                        "&:hover": {
+                            backgroundColor: "primary.light",
+                            transition: "background-color 0.5s ease",
+                        },
+                        transition: "background-color 0.5s ease",
+                    }}
+                >
+                    Codevolution
+                </Box>
+                <Box
+                    display="flex"
+                    height="100px"
+                    width="200px"
+                    bgcolor={"success.light"}
+                    p={2}
+                ></Box>
+            </Stack>
+            <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+                <Grid item xs={6}>
+                    <Box bgcolor={"primary.light"} p={2}>
+                        Item 1
+                    </Box>
+                </Grid>
+                <Grid item xs={6}>
+                    <Box bgcolor={"primary.light"} p={2}>
+                        Item 2
+                    </Box>
+                </Grid>
+                <Grid item xs={6}>
+                    <Box bgcolor={"primary.light"} p={2}>
+                        Item 3
+                    </Box>
+                </Grid>
+                <Grid item xs={6}>
+                    <Box bgcolor={"primary.light"} p={2}>
+                        Item 4
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
     );
 };
