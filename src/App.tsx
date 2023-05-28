0882d0a65d6186ea2d726396304b843d01beadbc
donoh <201904014@o.cnu.ac.kr>
2023-05-28 23:24:32 +0900

    feat : Customizing Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import { Stack } from "@mui/material";
+import { Stack, ThemeProvider, colors, createTheme } from "@mui/material";
 import { ToHome } from "./components/ToHome";
+
+const theme = createTheme({
+    status: {
+        danger: "#e53e3e",
+    },
+    palette: {
+        secondary: {
+            main: colors.orange[500],
+        },
+        neutral: {
+            main: colors.grey[500],
+            darker: colors.grey[700],
+        },
+    },
+});
 
 function App() {
     return (
-        <div className="App">
-            <Stack spacing={4} marginBottom={2}>
-                <ToHome />
-            </Stack>
-            <Outlet />
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <Stack spacing={4} marginBottom={2}>
+                    <ToHome />
+                </Stack>
+                <Outlet />
+            </div>
+        </ThemeProvider>
     );
 }
 
--- src/components/MuiResponsiveness.tsx
+++ src/components/MuiResponsiveness.tsx
@@ -1,20 +1,29 @@
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    height: "250px",
+    width: "250px",
+    backgroundColor: theme.palette.neutral?.darker,
+}));
 
 export const MuiResponsiveness = () => {
     return (
-        <Box
-            sx={{
-                height: 300,
-                width: {
-                    xs: 100, // 0px above
-                    sm: 200, // 600px above
-                    md: 300, // 900px above
-                    lg: 400, // 1200px above
-                    xl: 500, // 1536px above
-                },
-                bgcolor: "primary.main",
-                transition: "width 0.5s ease-out",
-            }}
-        ></Box>
+        <>
+            <Box
+                sx={{
+                    height: 300,
+                    width: {
+                        xs: 100, // 0px above
+                        sm: 200, // 600px above
+                        md: 300, // 900px above
+                        lg: 400, // 1200px above
+                        xl: 500, // 1536px above
+                    },
+                    bgcolor: "secondary.dark",
+                    transition: "width 0.5s ease-out",
+                }}
+            />
+            <StyledBox />
+        </>
     );
 };
